8e6af2f1ca1fd2e1a4f5026b1c5e6f36bab86ef6
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-11-24 15:28:16 +0100

    added dark mode and toggle input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <div className="App">
+      {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
+      <div className="toggle-switch">
+        <input
+          id="theme-switch"
+          type="checkbox"
+          className="toggle-switch-checkbox"
+          onChange={toggleTheme}
+          checked={theme === 'dark'}
+        />
+        <label className="toggle-switch-label" htmlFor="theme-switch">
+          <span className="toggle-switch-inner" />
+          <span className="toggle-switch-switch" />
+        </label>
+      </div>
       <h1>Hello World</h1>
     </div>
   );
