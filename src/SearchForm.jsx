f6b1607139d43182712b21b4f669e14380d2c13b
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-11-27 11:55:27 +0100

    added search form component

---
+++ src/SearchForm.jsx
@@ -0,0 +1,30 @@
+// SearchForm.js
+import React, { useState } from 'react';
+
+function SearchForm({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    onSearch(query); // Call the onSearch prop with the query
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        id="word-search"
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search for a word"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchForm;
