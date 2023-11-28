3bb461390933dc96f9305a5210a4fb6da5dc3ba5
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-11-28 18:21:52 +0100

    moved components to folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+import ThemeToggle from './ThemeToggle';
+
+function Header({ theme, toggleTheme }) {
+  return (
+    <header>
+      <h1>Dictionary</h1>
+      <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+    </header>
+  );
+}
+
+export default Header;
