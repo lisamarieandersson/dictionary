64cc1fbd95943ae67fad5037d9b022358605d6dc
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-11-30 02:35:59 +0100

    added test

--- src/tests/SearchForm.test.jsx
+++ src/tests/SearchForm.test.jsx
@@ -1,9 +1,12 @@
+// Unit tests for SearchForm component
+
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import SearchForm from '../components/SearchForm';
 
-test('should accept a user typing', async () => {
+// Verifying that the input field correctly accepts and displays user-typed text
+test('should allow user to type in the search input', async () => {
   render(<SearchForm />);
   const user = userEvent.setup();
   const searchInput = screen.getByRole('textbox');
@@ -11,4 +14,47 @@ test('should accept a user typing', async () => {
   await user.type(searchInput, 'asd');
 
   expect(searchInput).toHaveValue('asd');
+});
+
+// Triggers the search function with the input value when the form is submitted
+test('should call onSearch with input value on submission', async () => {
+  const mockOnSearch = vi.fn();
+  render(<SearchForm onSearch={mockOnSearch} />);
+  const user = userEvent.setup();
+
+  const searchInput = screen.getByRole('textbox');
+  const searchButton = screen.getByRole('button', { name: /search/i });
+
+  await user.type(searchInput, 'test');
+  await user.click(searchButton);
+
+  expect(mockOnSearch).toHaveBeenCalledWith('test');
+});
+
+test('should display a validation error for empty submission via click', async () => {
+  const mockOnSearch = vi.fn();
+  render(<SearchForm onSearch={mockOnSearch} />);
+  const user = userEvent.setup();
+
+  const searchButton = screen.getByRole('button', { name: /search/i });
+  await user.click(searchButton);
+
+  expect(
+    screen.getByText(/Please enter a word to search\./i)
+  ).toBeInTheDocument();
+  expect(mockOnSearch).not.toHaveBeenCalled();
+});
+
+test('should display a validation error for empty submission via enter', async () => {
+  const mockOnSearch = vi.fn();
+  render(<SearchForm onSearch={mockOnSearch} />);
+  const user = userEvent.setup();
+
+  const searchInput = screen.getByRole('textbox');
+  await user.type(searchInput, '{Enter}');
+
+  expect(
+    screen.getByText(/Please enter a word to search\./i)
+  ).toBeInTheDocument();
+  expect(mockOnSearch).not.toHaveBeenCalled();
 });
