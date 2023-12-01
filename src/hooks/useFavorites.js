8b4dfcef291a46358b4c1dc5ad41243a14904f08
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-01 16:32:11 +0100

    edited useFavorites to save wordData instead of word in sessionStorage

--- src/hooks/useFavorites.js
+++ src/hooks/useFavorites.js
@@ -7,21 +7,27 @@ function useFavorites() {
   });
 
   useEffect(() => {
+    // Save favorites to sessionStorage whenever they change
     sessionStorage.setItem('favorites', JSON.stringify(favorites));
   }, [favorites]);
 
-  const addFavorite = (word) => {
-    if (!favorites.includes(word)) {
-      setFavorites([...favorites, word]);
+  const addFavorite = (wordData) => {
+    const isAlreadyFavorite = favorites.some(
+      (fav) => fav.word === wordData.word
+    );
+    if (!isAlreadyFavorite) {
+      setFavorites((prevFavorites) => [...prevFavorites, wordData]);
     }
   };
 
-  const removeFavorite = (word) => {
-    setFavorites(favorites.filter((fav) => fav !== word));
+  const removeFavorite = (wordToRemove) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.filter((fav) => fav.word !== wordToRemove)
+    );
   };
 
-  const isFavorite = (word) => {
-    return favorites.includes(word);
+  const isFavorite = (wordToCheck) => {
+    return favorites.some((fav) => fav.word === wordToCheck);
   };
 
   return { favorites, addFavorite, removeFavorite, isFavorite };
