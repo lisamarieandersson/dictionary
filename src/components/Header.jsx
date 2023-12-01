e883928f6ea6e3d47568e0445a82db92c20108ae
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-01 08:35:12 +0100

    added favorites button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import styles from './Header.module.css';
 import ThemeToggle from './ThemeToggle';
 
-function Header({ theme, toggleTheme }) {
+function Header({ theme, toggleTheme, toggleView, showFavorites }) {
   return (
-    <header className={styles['header']}>
+    <header className={styles['header-container']}>
       <h1>Dictionary</h1>
-      <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+      <div className={styles['header-button-container']}>
+        <button
+          className={styles['header-toggle-favorites-back-to-search-button']}
+          onClick={toggleView}
+        >
+          {showFavorites ? 'Back to Search' : 'Favorites'}
+        </button>
+        <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+      </div>
     </header>
   );
 }
