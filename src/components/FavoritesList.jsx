05df5c704da16c031c4e38306097d2afe9b82b9a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-01 16:31:44 +0100

    edited favorites list for word data

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -6,12 +6,26 @@ function FavoritesList({ favorites, removeFavorite }) {
     <div className={styles['favorite-list-container']}>
       <h3>Favorites</h3>
       <ul>
-        {favorites.map((word) => (
-          <li className={styles['favorite-list-item']} key={word}>
-            {word}
+        {favorites.map((favorite) => (
+          <li className={styles['favorite-list-item']} key={favorite.word}>
+            <strong>{favorite.word}</strong>
+            {/* Display the phonetics if available */}
+            {favorite.phonetics && favorite.phonetics.length > 0 && (
+              <p>Phonetic: {favorite.phonetics[0].text}</p>
+            )}
+            {/* Display meanings, only showing the first definition for simplicity */}
+            {favorite.meanings &&
+              favorite.meanings.map((meaning, idx) => (
+                <div key={idx}>
+                  <h5>{meaning.partOfSpeech}</h5>
+                  {meaning.definitions && meaning.definitions.length > 0 && (
+                    <p>Definition: {meaning.definitions[0].definition}</p>
+                  )}
+                </div>
+              ))}
             <button
               className={styles['favorite-remove-button']}
-              onClick={() => removeFavorite(word)}
+              onClick={() => removeFavorite(favorite.word)}
             >
               Remove
             </button>
