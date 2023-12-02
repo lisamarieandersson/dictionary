5c73511a16740ce437072774872669581b441679
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-03 00:23:39 +0100

    edited comment

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styles from './FavoriteList.module.css';
 
-// Renders and mananages the list of favorite words, with and word data
+// Renders and mananages the list of favorite words, with word data
 // Each favorite word can be expanded or collapsed to show or hide its details
+// A word can also be removed from the favorite list
 
 function FavoriteList({ favorites, removeFavorite }) {
   // State object to track the expanded status of each item
